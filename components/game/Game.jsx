7d60e87d629444dfdb478e234e5e2bb3afbea115
kkapi <hZG2fik_j8@mail.ru>
2023-07-06 21:41:01 +0300

    board

--- components/game/Game.jsx
+++ components/game/Game.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { SYMBOL_O, SYMBOL_X } from '../../utils/consts';
+import styles from '../../styles/game.module.css'
 
 export function Game() {
-  return (
-    <div>Game</div>
-  )
+	const cells = [
+		SYMBOL_X,
+		null,
+		null,
+		SYMBOL_O,
+		SYMBOL_X,
+		null,
+		null,
+		null,
+		null,
+	];
+    
+	const currentStep = SYMBOL_O;
+
+	return (
+		<div className={styles["game"]}>
+			<div className={styles["game-info"]}></div>
+			<div className={styles["game-field"]}>
+				{cells.map((cell, index) => (
+					<button key={index} className={styles["cell"]}>{currentStep}</button>
+				))}
+			</div>
+		</div>
+	);
 }
