811a6defcf06f2201fee95e0da1205654015d73a
kkapi <hZG2fik_j8@mail.ru>
2023-07-07 17:41:33 +0300

    display winner

--- components/game/Game.jsx
+++ components/game/Game.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import { SYMBOL_O, SYMBOL_X } from '../../utils/consts';
 import classes from '../../styles/game.module.css';
+import { computeWinner } from '../../utils/game';
 
 export function Game() {
 	const [cells, setCells] = useState(Array(9).fill(null));
-
 	const [currentStep, setCurrentStep] = useState(SYMBOL_X);
+	const [winnerSequence, setWinnerSequence] = useState();
 
 	const getSymbolClassName = symbol => {
 		if (symbol === SYMBOL_O) return 'symbol--o';
@@ -23,26 +24,33 @@ export function Game() {
 		</span>
 	);
 
+	const winnerSymbol = winnerSequence ? cells[winnerSequence[0]] : undefined;
+
 	const handleCick = index => {
-		if (cells[index]) return;
-		const newCells = cells.slice();
-		newCells[index] = currentStep;
-		setCells(newCells);
+		if (cells[index] || winnerSequence) return;
+		const cellsCopy = cells.slice();
+		cellsCopy[index] = currentStep;
+
+		const winner = computeWinner(cellsCopy);
+
+		setCells(cellsCopy);
 		setCurrentStep(currentStep === SYMBOL_X ? SYMBOL_O : SYMBOL_X);
+		setWinnerSequence(winner);
 	};
 
 	return (
 		<div className={classes['game']}>
 			<div className={classes['game-info']}>
-				Ход: {renderSymbol(currentStep)}
+				{winnerSequence ? 'Победитель: ' : 'Ход: '}{renderSymbol(winnerSymbol ?? currentStep)}
 			</div>
 			<div className={classes['game-field']}>
 				{cells.map((symbol, index) => {
+					const isWinner = winnerSequence?.includes(index);
 					return (
 						<button
 							onClick={() => handleCick(index)}
 							key={index}
-							className={`${classes['cell']}`}
+							className={`${classes['cell']} ${isWinner ? classes['cell--win'] : ''}`}
 						>
 							{symbol ? renderSymbol(symbol) : null}
 						</button>
