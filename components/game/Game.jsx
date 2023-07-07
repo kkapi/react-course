dc83a3cc218abf70afdf8e2b3b8c769105ab4043
kkapi <hZG2fik_j8@mail.ru>
2023-07-07 16:09:30 +0300

    game component

--- components/game/Game.jsx
+++ components/game/Game.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { SYMBOL_O, SYMBOL_X } from '../../utils/consts';
-import styles from '../../styles/game.module.css'
+import classes from '../../styles/game.module.css';
 
 export function Game() {
 	const cells = [
@@ -10,20 +10,35 @@ export function Game() {
 		SYMBOL_O,
 		SYMBOL_X,
 		null,
-		null,
+		SYMBOL_O,
 		null,
 		null,
 	];
-    
-	const currentStep = SYMBOL_O;
+	const currentStep = SYMBOL_X;
+
+	const getSymbolClassName = symbol => {
+		if (symbol === SYMBOL_O) return 'symbol--o';
+		if (symbol === SYMBOL_X) return 'symbol--x';
+		return '';
+	};
+
+	const renderSymbol = symbol => (
+		<span className={`${classes['symbol']} ${classes[getSymbolClassName(symbol)]}`}>
+			{symbol}
+		</span>
+	);
 
 	return (
-		<div className={styles["game"]}>
-			<div className={styles["game-info"]}></div>
-			<div className={styles["game-field"]}>
-				{cells.map((cell, index) => (
-					<button key={index} className={styles["cell"]}>{currentStep}</button>
-				))}
+		<div className={classes["game"]}>
+			<div className={classes["game-info"]}>Ход: {renderSymbol(currentStep)}</div>
+			<div className={classes["game-field"]}>
+				{cells.map((symbol, index) => {
+					return (
+						<button key={index} className={`${classes['cell']}`}>
+							{symbol ? renderSymbol(symbol) : null}
+						</button>
+					);
+				})}
 			</div>
 		</div>
 	);
